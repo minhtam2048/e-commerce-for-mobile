60b8a59aa7919896cc90fd35743dfe426810142a
minhtam2048 <minhtam2048@gmail.com>
2020-03-04 09:14:03 +0700

    added refresh indicator to user product screen

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -8,6 +8,10 @@ import 'package:shop_app/widgets/user_products_item_widget.dart';
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
 
+  Future<void> _refreshProduct(BuildContext context) async {
+    await Provider.of<ProductsProvider>(context,listen: false).fetchProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productData = Provider.of<ProductsProvider>(context);
@@ -25,21 +29,23 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawerWidget(),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemBuilder: (_, i) => Column(
-            children: [
-              UserProductItemWidget(
-                productData.items[i].id,
-                productData.items[i].title,
-                productData.items[i].imageUrl,
-              ),
-              Divider(),
-            ],
+      body: RefreshIndicator(
+              child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemBuilder: (_, i) => Column(
+              children: [
+                UserProductItemWidget(
+                  productData.items[i].id,
+                  productData.items[i].title,
+                  productData.items[i].imageUrl,
+                ),
+                Divider(),
+              ],
+            ),
+            itemCount: productData.items.length,
           ),
-          itemCount: productData.items.length,
-        ),
+        ), onRefresh: () => _refreshProduct(context),
       ),
     );
   }
